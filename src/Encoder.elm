d0b18d3bd528a4497196ea2cea386e6093d50ea5
Topi <topi.paavilainen@wfp.org>
2020-12-25 22:59:09 +0200

    encoding/decoding

--- src/Encoder.elm
+++ src/Encoder.elm
@@ -1,10 +1,12 @@
-module Encoder exposing (encodeTokenMap)
+module Encoder exposing (encodeTokenMap, decodeTokenMap)
 
 import Tokens exposing (Token)
 import TokenMap exposing (TokenMap)
 import Hexagons.Map exposing (Map)
 import Dict
 import List
+import Char
+import Char exposing (isDigit)
 
 
 type EncodingToken = Empty Int | NotEmpty Token
@@ -26,7 +28,7 @@ encodeTokenMap : Map -> TokenMap -> String
 encodeTokenMap hexMap tokenMap =
   let
     hexList = Dict.keys hexMap
-    encodedList = List.reverse (List.foldl (encodingHelper tokenMap) [] hexList)
+    encodedList = List.foldl (encodingHelper tokenMap) [] hexList
   in
     String.join "" (List.map 
       (\elem ->
@@ -34,3 +36,68 @@ encodeTokenMap hexMap tokenMap =
           Empty number -> String.fromInt number
           NotEmpty token -> Tokens.encodeToken token
       ) encodedList )
+
+encodingStringHelper : Char -> List String -> List String
+encodingStringHelper newChar tokenList =
+  let
+    tail =
+      case List.tail tokenList of
+        Just list -> list
+        Nothing -> []
+  in
+    case (List.head tokenList) of
+      Just head ->
+        case (Char.isDigit newChar, String.all Char.isDigit head) of
+          (True, True) -> [(String.fromChar newChar) ++ head] ++ tail
+          (False, False) -> [(String.fromChar newChar) ++ head] ++ tail
+          _ -> [String.fromChar newChar] ++ tokenList
+      Nothing ->
+        [String.fromChar newChar]
+
+stringToEncodingToken : String -> EncodingToken
+stringToEncodingToken str =
+  if (String.all Char.isDigit str)
+    then Empty (Maybe.withDefault 0 (String.toInt str))
+    else
+      case (Tokens.decodeToken str) of
+        Just token -> NotEmpty token
+        Nothing -> Empty 1
+
+
+addXEmpty : EncodingToken -> List EncodingToken -> List EncodingToken
+addXEmpty token tokenList =
+  case token of
+    Empty 1 ->
+      tokenList ++ [Empty 0]
+    Empty x ->
+      addXEmpty (Empty (x-1)) (tokenList ++ [Empty 0])
+    _ ->
+      [token]
+
+
+addEmpties : EncodingToken -> List EncodingToken -> List EncodingToken
+addEmpties token tokenList =
+  case token of
+    Empty x -> (addXEmpty token []) ++ tokenList
+    NotEmpty x -> token::tokenList
+
+
+toToken : ((Int, Int, Int), EncodingToken) -> Maybe ((Int, Int, Int), Token)
+toToken (location, encodingToken) =
+  case encodingToken of
+    NotEmpty token -> Just (location, token)
+    Empty _ -> Nothing
+
+
+decodeTokenMap : String -> Map -> TokenMap
+decodeTokenMap tokenStr hexMap =
+  let
+    tokenList = List.map stringToEncodingToken (List.foldr encodingStringHelper [] (String.toList tokenStr))
+    withEmpties = List.foldl addEmpties [] tokenList
+    hexList = Dict.keys hexMap
+    hexTokenList = List.map2 Tuple.pair hexList withEmpties
+    tokensOnly = List.filterMap toToken hexTokenList
+  in
+    Dict.fromList tokensOnly
+
+
